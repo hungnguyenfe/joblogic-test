23731948eb6eac0ca282f3f7f7db9064cecb0977
Hung Nguyen Minh <hung.nguyen14fe@gmail.com>
2024-06-09 19:44:54 +0700

    Fix invalid path import after cleaned up

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import { pinia } from '../store'
+import './style.css'
+import { createApp } from 'vue'
+import { pinia } from './store'
 
 createApp(App).use(pinia).mount('#app')
